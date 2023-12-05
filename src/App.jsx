0df45519072028bd164692a99c2acfc42f212f14
Othniel Aghey <othnielignacio@gmail.com>
2023-12-05 19:58:31 +0000

    under construction

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,55 @@ import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import UnderConstruction from 'react-under-construction';
+import 'react-under-construction/build/css/index.css';
+import bg from './assets/bg-texture.png';
+import logooa from './assets/logo/oa.png';
+import linkedin from './assets/social/linkedin.png';
+import instagram from './assets/social/instagram.png';
+import mailto from './assets/social/mail-inbox-app.png';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <Footer />
+      <UnderConstruction
+        background={{
+          color: bg,
+          textColor: '#fff',
+          overlay: {
+            color: '#000',
+            opacity: '.5'
+          }
+        }}
+        logo={{
+          src: logooa,
+          alt: 'alt text',
+          style: {
+            maxWidth: '440px',
+          }
+          
+        }}
+        title={{
+          text: 'Hello'
+        }}
+        description={{
+          text: 'My website is under construction. I\'ll be back soon.',
+        }}
+        links={[
+          {
+            url: 'https://www.linkedin.com/in/othnielaghey/',
+            image: linkedin,
+          },
+          {
+            url: 'https://www.instagram.com/othniel.aghey/',
+            image: instagram,
+          },
+          {
+            url: 'mailto:othnielignacio@gmail.com',
+            image: mailto,
+          },
+        ]}
+      />
     </>
   )
 }
